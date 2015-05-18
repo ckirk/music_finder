3fb722c2a1ef75d480dbb156990f601129738874
Chris <ckirkinis@gmail.com>
2015-05-18 17:32:20 -0400

    fixed column 'album' to 'track'
    (in query table)

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150518203001) do
+ActiveRecord::Schema.define(version: 20150518212059) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "queries", force: :cascade do |t|
     t.string   "artist"
-    t.string   "album"
+    t.string   "track"
     t.string   "query"
     t.integer  "duration"
     t.datetime "date_published"
